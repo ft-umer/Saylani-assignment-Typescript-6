98e431f424d5a391eebdfc991b3b3f3a358222a3
ft-umer <syedumerhassni@gmail.com>
2022-08-22 00:21:46 +0500

    third commit

---
+++ first.ts
@@ -0,0 +1,57 @@
+// var message:string = "Hello World"
+// var number:number = 1
+// console.log(message)
+// console.log(number)
+
+// °F = (°C × 9/5) + 32
+
+// var temp
+// var celcius:number = 98
+// console.log(celcius * 9/5 + 32)
+
+// var num:number = 18
+// if (num % 2 == 0) {
+//     console.log(num+ " is even")
+// } else {
+//     console.log(num+ " is odd")
+// }
+
+// var num:number = 1
+// var range = 10
+// for(var i = 1;i <= range; ++i) {
+//     console.log(num+ "*" +i+ "=" +i*num )
+// } 
+
+// var num:number = 4
+// if (num > 0) {
+//     console.log("male")   
+// }else if (num < 0) {
+//     console.log("female")
+// }else{
+//     console.log("neither male nor female")
+// }
+var userInput:string | null = prompt("Enter your number");
+var num:number = Number(userInput)
+var range = 10
+for(var i = 1;i <= range; ++i) {
+    console.log(num+ "*" +i+ "=" +i*num )
+} 
+// switch (grade) {
+//     case "A":
+//         console.log("Grade A")
+//         break;
+//     case "B":
+//         console.log("Grade B")
+//         break;
+//     case "C":
+//         console.log("Grade C")
+//         break;
+//     case "D":
+//         console.log("Grade D")
+//         break;
+//     default:
+//         console.log("Its not A, B, C and D")
+//         break;
+// }
+
+
